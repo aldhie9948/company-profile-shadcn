14e93415aaf5dcb239a33b118d470eb97356bee9
Aldi Gunawan <it.aldigunawan@gmail.com>
2025-03-16 19:24:01 +0700

    feat: add dynamic canonical link to page head and remove hardcoded link from app.html

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,7 +4,12 @@
 	import SectionContactUs from "./section-contact-us.svelte";
 	import SectionHero from "./section-hero.svelte";
 	import SectionProducts from "./section-products.svelte";
+	import { env } from "$env/dynamic/public";
 </script>
+
+<svelte:head>
+	<link rel="canonical" href={env.PUBLIC_HOST_DOMAIN} />
+</svelte:head>
 
 <SectionHero />
 <SectionAboutUs />
